d0b08ca218db73daf0318e8356c52cb48218033d
ster4runsneakers <cornerstore553@gmail.com>
2025-10-11 02:02:06 +0300

    Upgrade service worker to version 3 and add sneakers.html

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE = 'ai-hub-v1';
+const CACHE = 'ai-hub-v3';
 const ASSETS = [
   './',
   './index.html',
+  './sneakers.html',
   './styles.css',
   './apps.json',
   './static/icons/icon-192.png',
@@ -14,15 +15,17 @@ self.addEventListener('install', (e)=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', (e)=>{
   e.waitUntil(
     caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE && caches.delete(k))))
   );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', (e)=>{
   e.respondWith(
-    caches.match(e.request).then(res => 
+    caches.match(e.request).then(res =>
       res || fetch(e.request).then(r=>{
         const copy = r.clone();
         caches.open(CACHE).then(c=> c.put(e.request, copy)).catch(()=>{});
