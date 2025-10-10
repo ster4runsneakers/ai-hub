eb3850790f740f8070a1adeb0b09c25b0cac7814
ster4runsneakers <cornerstore553@gmail.com>
2025-10-11 02:09:38 +0300

    Update service worker cache version to v4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ai-hub-v3';
+const CACHE = 'ai-hub-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -15,14 +15,10 @@ self.addEventListener('install', (e)=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE && caches.delete(k))))
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE && caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', (e)=>{
   e.respondWith(
     caches.match(e.request).then(res =>
