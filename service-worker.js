13df99f4d55fb8d77c30f757b2b42c91536005d8
ster4runsneakers <cornerstore553@gmail.com>
2025-10-11 01:48:17 +0300

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,9 @@ self.addEventListener('install', (e)=>{
   self.skipWaiting();
 });
 self.addEventListener('activate', (e)=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE && caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE && caches.delete(k))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch', (e)=>{
